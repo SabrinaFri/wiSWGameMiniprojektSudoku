f403e763e302f91209e8b5d376249b278a2d6fca
Sabrina <Sabrina@DESKTOP-Q9CKH27.fritz.box>
2020-04-17 22:41:49 +0200

    Cell class

--- src/wiSWGameMiniprojektSudoku/SudokuView/Cell.java
+++ src/wiSWGameMiniprojektSudoku/SudokuView/Cell.java
@@ -1,5 +1,35 @@
 package wiSWGameMiniprojektSudoku.SudokuView;
 
-public class Cell {
+import javafx.scene.control.TextField;
+
+public class Cell extends TextField{
+	public Cell() {
+		super();
+		
+		setTypeNormal();
+	}
+
+	private void setTypeNormal() {
+		setEditable(false);
+		getStyleClass().add("normalCell");
+	}
+	
+	public void setEditable() {
+		setEditable(true);
+		getStyleClass().add("editCell");
+
+	}
+	
+	public void setEditableCorrect() {
+		setEditable(true);
+		getStyleClass().add("correctCell");
+
+	}
+	
+	public void setEditableInorrect() {
+		setEditable(true);
+		getStyleClass().add("incorrectCell");
+	}
+	
 
 }
